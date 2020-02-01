bd38fe3221902f0ed571ab9fb0d24b9994e619ca
Jesse <jessepatch9@gmail.com>
2020-02-01 14:55:30 -0600

    Add routing to spell tracker component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SpellTrackerComponent } from './spell-tracker/spell-tracker.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'spell-tracker', component: SpellTrackerComponent}
 ];
 
 @NgModule({
